88dddba95a8318664812e07bf13387ccc5f2efa2
Anton Gushcha <ncrashed@gmail.com>
2017-03-02 03:40:38 +0300

    Reexport encode module as internal

--- src/Data/SafeCopy/Internal.hs
+++ src/Data/SafeCopy/Internal.hs
@@ -1,7 +1,9 @@
 module Data.SafeCopy.Internal (
     module Data.SafeCopy.SafeCopy
+  , module Data.SafeCopy.Encode
   , module Data.SafeCopy.Derive
   ) where
 
-import Data.SafeCopy.SafeCopy
 import Data.SafeCopy.Derive
+import Data.SafeCopy.Encode
+import Data.SafeCopy.SafeCopy
